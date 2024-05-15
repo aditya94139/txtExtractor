163073395d840f8d52f1b70cfa8152c8f5b75788
HexaTalkMain <166362829+HexaTalkMain@users.noreply.github.com>
2024-05-16 01:24:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6618274486:AAESKaMTJJ0tOR62pqOPt-a0WcfpTi0y7nk")
-    API_ID = int(os.environ.get("API_ID", "20089019"))
-    API_HASH = os.environ.get("API_HASH", "0b2eb6aa72d5983fa9e4b8c77791c9c4")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6973701854:AAE2zRZAK23LkKJbAC7dJ84eVZonwkwar2A")
+    API_ID = int(os.environ.get("API_ID", "23537723"))
+    API_HASH = os.environ.get("API_HASH", "165f8ac57a0fc673437772bbffd315a4")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "6767962475")
